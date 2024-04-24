15407dc59b58146160f536e31871341d77ea9300
CherYar <113169774+CherYar@users.noreply.github.com>
2024-04-24 11:17:20 +0300

    Create TopSecretSafePlus.php

---
+++ TopSecretSafePlus.php
@@ -0,0 +1,8 @@
+<?php
+    if ($_COOKIE['sessionid'] == '261201081285') {
+        // Выводим сообщение об ошибке или перенаправляем пользователя
+        echo "Access denied";
+    } else {
+        echo "Access denied";
+    }
+?>
