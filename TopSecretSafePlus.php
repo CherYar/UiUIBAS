9ab83eb4d83bdce13abbb9c8dc8dff3ca4bcc221
CherYar <yarslvchern@gmail.com>
2024-04-30 12:48:18 +0300

    Начало 11й практики, старые файлы убраны из новой ветки

--- TopSecretSafePlus.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if ($_COOKIE['sessionid'] == '261201081285') {
-        // Выводим сообщение об ошибке или перенаправляем пользователя
-        echo "Access denied";
-    } else {
-        echo "Access denied";
-    }
-?>
